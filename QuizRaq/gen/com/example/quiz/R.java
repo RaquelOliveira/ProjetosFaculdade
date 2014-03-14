ee2feacb7e45d7ffa800a7b4f6c1552cc25a6e40
Raquel Oliveira <ods.raquel@gmail.com>
2014-03-14 20:13:19 -0300

    atualização na tela de resultado

--- QuizRaq/gen/com/example/quiz/R.java
+++ QuizRaq/gen/com/example/quiz/R.java
@@ -24,34 +24,35 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001b;
+        public static final int action_settings=0x7f08001c;
         public static final int button1=0x7f080006;
-        public static final int button2=0x7f080014;
-        public static final int button3=0x7f08001a;
-        public static final int button4=0x7f080007;
-        public static final int pergunta2=0x7f08000e;
+        public static final int button2=0x7f080015;
+        public static final int button3=0x7f08001b;
+        public static final int button4=0x7f080008;
+        public static final int pergunta2=0x7f08000f;
         public static final int radio1=0x7f080002;
-        public static final int radio1Act2=0x7f080010;
-        public static final int radio1Act3=0x7f080016;
-        public static final int radio1Act4=0x7f080009;
+        public static final int radio1Act2=0x7f080011;
+        public static final int radio1Act3=0x7f080017;
+        public static final int radio1Act4=0x7f08000a;
         public static final int radio2=0x7f080003;
-        public static final int radio2Act2=0x7f080011;
-        public static final int radio2Act3=0x7f080017;
-        public static final int radio2Act4=0x7f08000a;
+        public static final int radio2Act2=0x7f080012;
+        public static final int radio2Act3=0x7f080018;
+        public static final int radio2Act4=0x7f08000b;
         public static final int radio3=0x7f080004;
-        public static final int radio3Act2=0x7f080012;
-        public static final int radio3Act3=0x7f080018;
-        public static final int radio3Act4=0x7f08000b;
+        public static final int radio3Act2=0x7f080013;
+        public static final int radio3Act3=0x7f080019;
+        public static final int radio3Act4=0x7f08000c;
         public static final int radio4=0x7f080005;
-        public static final int radio4Act2=0x7f080013;
-        public static final int radio4Act3=0x7f080019;
-        public static final int radio4Act4=0x7f08000c;
+        public static final int radio4Act2=0x7f080014;
+        public static final int radio4Act3=0x7f08001a;
+        public static final int radio4Act4=0x7f08000d;
         public static final int radioGroup1=0x7f080001;
-        public static final int radioGroup2=0x7f08000f;
-        public static final int radioGroup3=0x7f080015;
-        public static final int radioGroup4=0x7f080008;
+        public static final int radioGroup2=0x7f080010;
+        public static final int radioGroup3=0x7f080016;
+        public static final int radioGroup4=0x7f080009;
+        public static final int textResult=0x7f08000e;
         public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f08000d;
+        public static final int textView2=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -106,6 +107,7 @@ public final class R {
         public static final int quest2=0x7f05000a;
         public static final int quest3=0x7f050012;
         public static final int quest4=0x7f05001a;
+        public static final int resultado=0x7f050022;
         public static final int title_activity_quarta=0x7f050019;
         public static final int title_activity_resultado=0x7f050021;
         public static final int title_activity_segunda=0x7f050009;
--- QuizRaq/src/com/example/quiz/ResultadoActivity.java
+++ QuizRaq/src/com/example/quiz/ResultadoActivity.java
@@ -3,13 +3,23 @@ package com.example.quiz;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.TextureView;
+import android.widget.TextView;
 
 public class ResultadoActivity extends Activity {
+	
+	private TextView textResult;
+	private String resp;
+	int result;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_resultado);
+		result= this.getIntent().getIntExtra("pontuacao", 0);
+		resp= "Fim de jogo! Sua Pontuacao e "+ Integer.toString(result);
+		textResult= (TextView) findViewById(R.id.textResult);
+		textResult.setText(resp);
 	}
 
 	@Override
@@ -17,6 +27,9 @@ public class ResultadoActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.resultado, menu);
 		return true;
+	
+		
+		
 	}
 
 }
